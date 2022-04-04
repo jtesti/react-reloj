0f48e61a78a112370a011912405be6dea9b86dda
jtesti <jtestill@gmail.com>
2022-04-05 01:10:53 +0200

    First update

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import './Clock.css';
+
+const date = new Date();
+
+export default function Clock() {
+  const [dateTime, setDateTime] = useState({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const date = new Date();
+      setDateTime({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="Clock">
+      <div>
+        {dateTime.hours}:{dateTime.minutes}:{dateTime.seconds}
+      </div>
+    </div>
+  );
+}
